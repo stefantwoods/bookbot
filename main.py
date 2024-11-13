031cead096cde8eca49ea9e7c11c9f1da0145f4a
stefantwoods <stefan5365@hotmail.com>
2024-11-12 21:22:46 -0700

    func that counts the num of words

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ with open(book_path, 'r') as f:
 
     # Print the content
     print(file_contents)
+
+# Count how many words the book contains
+num_words = 0
+
+with open(book_path, 'r') as f:
+    # Grab each line
+    for line in f:
+        words = line.split()
+
+        # Add the number of elements per line to the count
+        num_words = num_words + len(words)
+
+print(f"There are {num_words} words in the book!")
+    
